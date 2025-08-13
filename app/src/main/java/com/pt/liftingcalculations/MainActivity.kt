c75376288be37de495926f7715906134b759cb4d
puneett56 <puneet.t56@gmail.com>
2025-08-13 18:41:08 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/pt/liftingcalculations/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pt.liftingcalculations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.pt.liftingcalculations.ui.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colorScheme.background) {
+                HomeScreen()
+            }
+        }
+    }
+}
